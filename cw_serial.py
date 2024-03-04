4d6fe73e5107234080c044e2eb8f8da0cb034a82
Ketil Røed <ketilroe@eduroam-193-157-160-132.wlan.uio.no>
2024-03-04 15:40:07 +0100

    Updated plots

--- cw_serial.py
+++ cw_serial.py
@@ -7,17 +7,18 @@ import queue
 import time
 import pandas as pd
 import numpy as np
-COM_PORT1 = "/dev/tty.usbserial-120" 
-COM_PORT2 = "/dev/tty.usbserial-110"
+COM_PORT1 = "/dev/tty.usbserial-2110" 
+COM_PORT2 = "/dev/tty.usbserial-2120"
 
 
 # Create a command queue for key
 cmd_queue = queue.Queue(10)
 data_queue = queue.Queue(20)
 
-columns=["module","count","time","adc","sipmv","deadtime","temp"]
+max_value = 200
+columns=["module","count","time","adc","sipmv","deadtime","temp","rate"]
 bins = [0,10,20,30,40,50,60,70,80,90,100,110,120,130]
-bins = np.arange(0,200,5)
+bins = np.arange(0,max_value,5)
 
 #values = [0,0,0,0,0,0,0,0,0,0,0,0,0,0]
 #df_data = pd.DataFrame(columns=columns)
@@ -49,8 +50,12 @@ def decode_data(data):
         if data[0] != "#":
             data = data.split(" ")
             data = [float(x) for x in data]
+            # calculate count rate
+            data.append(1000*(data[0]/(data[1]-data[4])))
         else:       
             data = ""
+        
+
     return data
     #print(type(data))
     
@@ -105,7 +110,7 @@ def detector(run_app):
     global df_data
     print("starting detector")
     
-    columns=["module","count","time","adc","sipmv","deadtime","temp"]
+    columns=["module","count","time","adc","sipmv","deadtime","temp","rate"]
     #bins = [0,10,20,30,40,50,60,70,80,90,100,110,120,130]
 #values = [0,0,0,0,0,0,0,0,0,0,0,0,0,0]
     #df_data = pd.DataFrame(columns=columns)
@@ -121,7 +126,8 @@ def detector(run_app):
                 print("first")
                 df_data = pd.DataFrame(columns=columns)
             #if (True & True):
-
+            
+            #print(df_data)
             #if ( df_temp[ (df_temp["module"]==1) & (df_temp["count"] == 1.0) ] ):
             #    print("first")
             #print(df_temp)
@@ -155,20 +161,70 @@ def animate(oFrame):
     global bin_edges1
     global hist_m2
     global bin_edges2
-    
+    global max_value
     global bins
     global df_data
     #plt.clf()
     ax1.cla()
     ax2.cla()
+    ax3.cla()
+    ax4.cla()
+    ax5.cla()
+    ax6.cla()
+    ax1.grid(True)
+    ax2.grid(True)
+    ax3.grid(True)
+    ax4.grid(True)
+    ax5.grid(True)
+    ax6.grid(True)
 
-    ax1.set_title('Cosmic Watch 1')
-    ax1.set_xlabel('Voltage')
-    ax1.set_ylabel('[V]')
+
+    title1 = "Raw data (Max: {0} mV, Counts: {1}) ".format(df_data[df_data["module"]==1]["sipmv"].max(),df_data[df_data["module"]==1]["count"].max())
+    ax1.set_title(title1)
+    ax1.set_xlabel('Voltage [mV]')
+    ax1.set_ylabel('#Counts')
     
-    ax2.set_title('Cosmic Watch 2')
-    ax2.set_xlabel('Voltage')
-    ax2.set_ylabel('[V]')
+    title2 = "Coincidence data (Max: {0} mV, Counts: {1}) ".format(df_data[df_data["module"]==2]["sipmv"].max(),df_data[df_data["module"]==2]["count"].max())
+   
+    ax2.set_title(title2)
+    ax2.set_xlabel('Voltage [mV]')
+    ax2.set_ylabel('#Counts')
+
+    ax3.set_title('Raw rate')
+    ax3.set_xlabel('Time [s]')
+    ax3.set_ylabel('Counts/s')
+    
+
+    ax4.set_title('Coincidence rate')
+    ax4.set_xlabel('Time [s]')
+    ax4.set_ylabel('Counts/s')
+
+
+    ax5.set_title('Temperature')
+    ax5.set_xlabel('Time [s]')
+    ax5.set_ylabel('°C')
+
+    ax6.set_title('Temperature')
+    ax6.set_xlabel('Time [s]')
+    ax6.set_ylabel('°C')
+    
+
+    
+    if df_data[df_data["module"]==1]["sipmv"].max() > max_value:
+        max_value = df_data[df_data["module"]==1]["sipmv"].max()
+        bins = np.arange(0,max_value,5)
+    if df_data[df_data["module"]==2]["sipmv"].max() > max_value:
+        max_value = df_data[df_data["module"]==1]["sipmv"].max()
+        bins = np.arange(0,max_value,5)
+
+
+
+
+    #if df_data[df_data["module"]==1]["sipmv"] > max_value:
+    #    max_value = 
+
+
+    #print(df_data)
 
     try:
         df_data[df_data["module"]==1]["sipmv"].hist(bins=bins,ax=ax1,color="blue")
@@ -181,6 +237,64 @@ def animate(oFrame):
     except:
         #ax2.cla()
         ax2.plot([],[])
+
+    #print(df_data)
+    #try:
+        #count1 = df_data[df_data["module"]==1 ]["count"].
+
+    # Get the last 10 seconds of data    
+    duration = df_data[(df_data["module"]==1)]["time"].max()
+    if duration  > 0:
+        plot_window_floor = duration - 0
+    else:
+        plot_window_floor = 0
+    filter1 = (df_data["module"]==1) & (df_data[(df_data["module"]==1)]["time"] >= plot_window_floor  )
+    filter2 = (df_data["module"]==2) & (df_data[(df_data["module"]==2)]["time"] >= plot_window_floor  )
+
+    #print(df_data[df_data["module"]==1])
+    try:
+        rate1 = df_data[df_data["module"]==1]["rate"] #/25 # cm2
+        time1 = df_data[df_data["module"]==1]["time"]/1000
+        temp1 = df_data[df_data["module"]==1]["temp"]
+        ax3.plot(time1,rate1,color="blue")
+        ax5.plot(time1,temp1,color="blue")
+
+        ax3.set_ylim([0, 6])
+        ax5.set_ylim([0, 30])
+    except:
+        ax3.plot([],[],color="blue")
+        ax5.plot([],[],color="blue")
+    
+    try:
+        rate2 = df_data[df_data["module"]==2]["rate"] #/25 # cm2
+        time2 = df_data[df_data["module"]==2]["time"]/1000
+        temp2 = df_data[df_data["module"]==2]["temp"]
+        ax4.plot(time2,rate2,color="blue")
+        ax6.plot(time2,temp2,color="blue")
+        #print(time2)
+        ax4.set_ylim([0, 0.5])
+        ax6.set_ylim([0, 30])
+    except:
+        ax4.plot([],[],color="blue")
+        ax6.plot([],[],color="blue")
+
+
+   
+
+    #time1 = df_data[filter]["time"]
+    #deadtime1 = df_data[filter]["time"]
+    #count1 =  df_data[filter]["time"]
+        #deadtime1 = df_data[df_data["module"]==1]["deadtime"]
+        #len1 = len(count1)
+
+        #print(count1[0:])
+        #df_data[df_data["module"]==1]["sipmv"].hist(bins=bins,ax=ax2,color="blue")
+
+        
+
+
+    #except:
+    #    ax3.plot([],[])
 
     #    print(bins)
     #    print(hist_m1)
@@ -200,16 +314,36 @@ def animate(oFrame):
 if __name__ == "__main__":
 
 
-    fig, [ax1,ax2] = plt.subplots(1,2)
-    #fig.set_tight_layout()
-    ax1.set_title('Cosmic Watch 1')
-    ax1.set_xlabel('Voltage')
-    ax1.set_ylabel('[V]')
+    fig, [(ax1, ax2), (ax3, ax4),(ax5, ax6)] = plt.subplots(3,2,gridspec_kw={'height_ratios': [4, 2,2]})
+    fig.suptitle('Cosmic Watch', fontsize=16)
+    fig.set_tight_layout(True) 
+    ax1.set_title('Raw data')
+    ax1.set_xlabel('Voltage [mV]')
+    ax1.set_ylabel('#Counts')
     
-    ax1.set_title('Cosmic Watch 2')
-    ax1.set_xlabel('Voltage')
-    ax1.set_ylabel('[V]')
+    ax2.set_title('Coincidence data')
+    ax2.set_xlabel('Voltage [mV]')
+    ax2.set_ylabel('#Counts')
     
+    ax3.set_title('Rate rate')
+    ax3.set_xlabel('Time [s]')
+    ax3.set_ylabel('Counts/s')
+    
+
+    ax4.set_title('Coincidence rate')
+    ax4.set_xlabel('Time [s]')
+    ax4.set_ylabel('Counts/s')
+
+    ax5.set_title('Temperature')
+    ax5.set_xlabel('Time [s]')
+    ax5.set_ylabel('°C')
+
+    ax6.set_title('Temperature')
+    ax6.set_xlabel('Time [s]')
+    ax6.set_ylabel('°C')
+    
+
+
     #ax.set_xlim( 0, 130)
     #ax.set_ylim( 0, 10)
     ax1.plot([],[])
@@ -233,6 +367,7 @@ if __name__ == "__main__":
     # Start threads
     #keyboard_thread.start()
     serial_thread1.start()
+    time.sleep(0.5)
     serial_thread2.start()
     
     detector_thread.start()
